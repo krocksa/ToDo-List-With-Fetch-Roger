b1f45ad1a0e300d70b7bcf0f5c649047bbc30b4f
krocksa <rogeryonatan@gmail.com>
2022-09-25 23:24:59 +0000

    Esto no es de aquí

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	const [ input, setInput ] = useState('');
+	const [ tasks, setTasks ] = useState([]);
+	const [ status, setStatus ] = useState('to-do');
+
+	function handleEnter(parameter) {
+		if (parameter.key === 'Enter') {
+				
+			setTasks([...tasks,{label:input,status:status}]);
+			setInput("")
+		}
+	}
+
+	function handleDelete(currentIndex) {
+		let newTasks = tasks.filter((task, index) => index != currentIndex );
+		setTasks(newTasks);
+	}
+
+	function handleUpdate(e,currentIndex) {
+		let newTasks = tasks.map((task, index) => {
+			
+			if(index == currentIndex){
+				return{...task,status:e.target.value}
+			}
+			else return task;
+});
+		setTasks(newTasks);
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			<input type="text" 
+				onChange={(e)=>{setInput(e.target.value)}}
+				value={input}
+				onKeyDown={handleEnter}
+			/>
+
+			<select onChange={(e) => 
+				{setStatus(e.target.value)}}
+				name="taskStatus" id="takStatus">
+				<option value="to do">To Do</option>
+				<option value="progress">In Progress</option>
+				<option value="done">Done</option>
+			</select>
+
+			<ListGroup>
+				{tasks.map((task,index) => {
+						return (
+						<ListGroup.Item  className="d-flex list" key={index}>
+							<p>
+							{task.label}
+							</p>
+							<select onChange={(e)=> handleUpdate(e,index)} name="taskStatus" id="takStatus">
+								<option value="to-do">To Do</option>
+								<option value="progress">In Progress</option>
+								<option value="done">Done</option>
+							</select>
+							<button className="button" onClick={(e)=> handleDelete(e,index)}>								
+							<span>
+								<i>X</i>
+							</span>
+							</button>
+							</ListGroup.Item>);
+				})}
+			</ListGroup>
 		</div>
 	);
 };
