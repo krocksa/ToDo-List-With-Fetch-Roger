2618754037749df447e61a210b30107a2e58057d
krocksa <rogeryonatan@gmail.com>
2022-09-27 04:13:53 +0000

    Ahora empieza a tener sentido

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,80 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	
-	const [ input, setInput ] = useState('');
-	const [ tasks, setTasks ] = useState([]);
-	const [ status, setStatus ] = useState('to-do');
 
-	function handleEnter(parameter) {
-		if (parameter.key === 'Enter') {
-				
-			setTasks([...tasks,{label:input,status:status}]);
-			setInput("")
-		}
-	}
+	const [ generaciones, setGeneraciones ] = useState([]);
 
-	function handleDelete(currentIndex) {
-		let newTasks = tasks.filter((task, index) => index != currentIndex );
-		setTasks(newTasks);
-	}
+	useEffect(() => {
 
-	function handleUpdate(e,currentIndex) {
-		let newTasks = tasks.map((task, index) => {
-			
-			if(index == currentIndex){
-				return{...task,status:e.target.value}
-			}
-			else return task;
-});
-		setTasks(newTasks);
-	}
-
+	fetch("https://pokeapi.co/api/v2/generation").then(resp => resp.json()).then(data => setGeneraciones(data.results))
+},[]);
 	return (
 		<div className="text-center">
-			
-			<input type="text" 
-				onChange={(e)=>{setInput(e.target.value)}}
-				value={input}
-				onKeyDown={handleEnter}
-			/>
-
-			<select onChange={(e) => 
-				{setStatus(e.target.value)}}
-				name="taskStatus" id="takStatus">
-				<option value="to do">To Do</option>
-				<option value="progress">In Progress</option>
-				<option value="done">Done</option>
-			</select>
-
-			<ListGroup>
-				{tasks.map((task,index) => {
-						return (
-						<ListGroup.Item  className="d-flex list" key={index}>
-							<p>
-							{task.label}
-							</p>
-							<select onChange={(e)=> handleUpdate(e,index)} name="taskStatus" id="takStatus">
-								<option value="to-do">To Do</option>
-								<option value="progress">In Progress</option>
-								<option value="done">Done</option>
-							</select>
-							<button className="button" onClick={(e)=> handleDelete(e,index)}>								
-							<span>
-								<i>X</i>
-							</span>
-							</button>
-							</ListGroup.Item>);
-				})}
-			</ListGroup>
+			{generaciones.map(generacion =>{
+				return <p>{generacion.name}</p>
+			})}
 		</div>
-	);
+	)
 };
 
 export default Home;
