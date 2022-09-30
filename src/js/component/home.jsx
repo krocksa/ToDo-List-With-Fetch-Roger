d281201878bd8711afd7d49974d976d4cd0644e2
krocksa <rogeryonatan@gmail.com>
2022-09-30 12:06:39 +0000

    Avanzando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,129 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 
-
-
-//create your first component
 const Home = () => {
 
-	const [ generaciones, setGeneraciones ] = useState([]);
-
-	useEffect(() => {
-
-	// fetch("https://pokeapi.co/api/v2/generation").then(resp => resp.json()).then(data => setGeneraciones(data.results))
-
-	const postToDo = async () => {
-		
-		let response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/krocksa",{
-			headers:{
-				"Content-Type": "application/json"
-			},
-			method: "POST",
-			body: JSON.stringify([])
-		});
-		let data = await response.json();
-		console.log(data);
-		
-	}
-	postToDo()
-	
-},[]);
-
-useEffect(() => {
-
-	// fetch("https://pokeapi.co/api/v2/generation").then(resp => resp.json()).then(data => setGeneraciones(data.results))
-
-	const putToDo = async () => {
-		
-		let response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/krocksa",{
-			headers:{
-				"Content-Type": "application/json"
-			},
-			method: "PUT",
-			body: JSON.stringify( [
-				{ label: "Make the bed", done: false },
-				{ label: "Walk the dog", done: false },
-				{ label: "Do the replits", done: false }
-			  ])
-		});
-		let data = await response.json();
-		setInputValue(data);
-		
-	}
-	putToDo()
-
-},[]);
-
-useEffect(() => {
-
-	// fetch("https://pokeapi.co/api/v2/generation").then(resp => resp.json()).then(data => setGeneraciones(data.results))
-
-	const getToDo = async () => {
-		
-		let response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/krocksa",{
-			headers:{
-				"Content-Type": "application/json"
-			},
-			method: "GET"
-		});
-		let data = await response.json();
-		console.log(data);
-		
-	}
-	getToDo()
-
-},[]);
-
-
-const [ inputValue, setInputValue ] = useState('');
+	const [ input, setInput ] = useState('');
 	const [ all, setAll ] = useState([]);
+	const [status, setStatus] = useState("to-do");
 
-	function handleChange(parameter) {
-		const value = parameter.target.value;
-		setInputValue(value);
-	}
-
-	function handleAdd(parameter) {
-		let copyAll = [...all];
-		if (parameter.key === 'Enter') {
-				
-			copyAll.push(inputValue);
-			setAll(copyAll);
-			setInputValue("")
+	function handleAdd(e) {
+		if (e.key == "Enter"){
+			setAll([...all,{label:input,status:status}])
+			setInput("")
 		}
 	}
-	function handleDelete(parameter) {
-		let copyAll = [...all];
-		copyAll.pop(inputValue);
-		setAll(copyAll);
+
+	const handleDelete = (currentIndex)=>{
+		let newTasks = all.filter((task, index)=> index != currentIndex)
+		setAll(newTasks)
 	}
 
 	return (
-		<div className="todolist">
-			<div className="dentroDe">
-				<h1>ALL TASKS</h1>
-			<div className="input">
-				<input type="text" placeholder="Input Task"
-				 value={inputValue} onChange={handleChange} 
-				 onKeyDown={handleAdd} />
-				<button onClick={handleAdd}>Add</button>
-			</div>
-					<div className="text-center">
-						{generaciones.map(generacion =>{
-							return <p>{generacion.name}</p>
-						})}
+		<div className="title">
+			<h2>ToDo List Roger.js</h2>
+				<div className="todolist">
+					<div className="dentroDe">
+						<h3>ALL TASKS</h3>
+					<div className="input">
+					<input 
+						type="text" placeholder="Input Task"
+						onChange={(e)=>{setInput(e.target.value)}} 
+						value={input} 
+						onKeyDown={handleAdd}
+					/>
 					</div>
-				{
-					all.map((one,index) => {
-						return(
-							<div className="newOne" key={index}>								
-								{one}
-								<button className="button" onClick={handleDelete}>X</button>
-							</div>
-						)
-					})
-			
-				}
+					<ListGroup>
+						{
+							all.map((task,index)=>{
+								return (
+									<ListGroup.Item className="newOne" key={index} >
+										<p>
+											{task.label}
+										</p>
+
+										<button className="button" onClick={(e) => handleDelete(index)}>
+											<span><i>X</i></span>
+										</button>
+									</ListGroup.Item>
+							)})
+						}
+					</ListGroup>
+				</div>
 			</div>
 		</div>
 	);
